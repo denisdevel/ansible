bc4025de2daa2de3cabaeb2d1bbf722d1b949ffe
root <denis.devel@gmail.com>
2018-05-23 04:47:32 +0000

    Centos

--- scripts/addnode.sh
+++ scripts/addnode.sh
@@ -8,6 +8,6 @@
 /bin/systemctl stop kubelet.service && /bin/rm -f /etc/kubernetes/bootstrap-kubelet.conf && /bin/rm -f /etc/kubernetes/kubelet.conf && /bin/rm -f /etc/kubernetes/bootstrap-kubelet.conf && /bin/rm -f /etc/kubernetes/pki/ca.crt && /bin/systemctl start kubelet.service
 
 # "include the node to the kubernate cluster"
-kubeadm join 172.31.100.1:6443 --token 41kjfi.etv03l6q1haybzxe --discovery-token-ca-cert-hash sha256:9e1057c625555fcbaac0a9a02c7069ae7f6245db41a6f4343bba4ecdac525a85
+kubeadm join 172.31.100.1:6443 --token ckk4s7.wcccip0g5pg90gn8 --discovery-token-ca-cert-hash sha256:c24cc1e95525b049c087740c0e71887b1c92328d0c5ef717d12af733e50f7f56
 
 
